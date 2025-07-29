92ddf1eb4ae2f579aade2b3e505261e0444c87ad
Yash Verma <151985213+yashverma8887@users.noreply.github.com>
2025-07-29 17:08:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-alert("Welcome to Yash's first website")
+console.log("Hey I am Yash here!! ")
+
+let a = 5;
+let b = 6;
+let c = "Yash";
+let _a = "Shubham";
+// var 55a = "Rohan";  //not allowed
+
+//console.log(a+b+10)
+//console.log(typeof a, typeof b, typeof c)
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+//const a1 = 6;
+// a1 = a1 + 1; // will not change... not allowed bcoz of constant does not change
+
+
+let x = "Yash Verma"; //string
+let y = 23;          // number
+let z = 3.25;        // number
+const p = true;      // boolean
+let q = undefined;   //undefined
+let r = null;        //object
+
+console.log(x , y, z, p, q, r)
+console.log(typeof x , typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+// object in js is key value pair
+let o = {
+    name : "Yash Verma",
+    age : 22,
+    "job code" : 22095,
+    "is_handsome" : true,
+}
+console.log(o)
+o.salary = "10crores"
+console.log(o)
